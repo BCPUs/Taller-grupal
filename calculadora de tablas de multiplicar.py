8dcf15bcda01688a09a2e2315d5f89dda34bea55
blcastillop <blcastillop@puce.edu.ec>
2024-12-14 12:40:46 -0500

    carga

--- calculadora de tablas de multiplicar.py
+++ calculadora de tablas de multiplicar.py
@@ -4,7 +4,7 @@ numero= int(input("Ingrese un numero para empezar: "))
 rango_inicial= int(input("Ingrese el rango inicial: ")) 
 rango_final= int(input("Ingrese el rango final: ")) 
 
-if rango_final>=rango_inicial:
+if rango_final>rango_inicial:
     print ("el rango inicial no puede ser mayor al rango final")
 else:
     print(f"tabla de multiplicar del {numero} desde {rango_inicial} hasta {rango_final}")
