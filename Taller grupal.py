d4aa7ad9fee42407c229c14980902b11ae5a1134
blcastillop <blcastillop@puce.edu.ec>
2024-12-14 12:24:11 -0500

    cargamultiplicacion

--- Taller grupal.py
+++ Taller grupal.py
@@ -1,1 +1,14 @@
-#asdas
+#tabla de multiplicar
+print("Biernvenido a este programa")
+numero= int(input("Ingrese un numero para empezar: ")) 
+rango_inicial= int(input("Ingrese el rango inicial: ")) 
+rango_final= int(input("Ingrese el rango final: ")) 
+
+if rango_final>rango_inicial:
+    print ("el rango inicial no puede ser mayor al rango final")
+else:
+    print(f"tabla de multiplicar del {numero}")
+    for i in range(rango_inicial,rango_final +1):
+        print(f"{numero}x{i}={numero*i}")
+
+
